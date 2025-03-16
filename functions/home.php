1c9af665a257d03034a8eee06d5becff3e8a554d
murrilop <murilogonzagasilva14@gmail.com>
2025-03-16 03:28:02 -0300

    back-end de portfolio e servicos atualizados

--- functions/home.php
+++ functions/home.php
@@ -1,13 +1,22 @@
 <?php
 
-include("../config/conexao.php");
-
 function mostrarServicos(){
+    global $pdo;
     $sql = "SELECT * FROM servico";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
 
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function mostrarImagemPortfolio(){
+    global $pdo;
+    $sql = "SELECT portfolio FROM funcionario";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 }
 
 ?>
