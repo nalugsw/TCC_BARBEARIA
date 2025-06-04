3e2387e5e9d6d3108baaeca5c52e9cec82fca47a
murrilop <murilogonzagasilva14@gmail.com>
2025-06-03 22:20:34 -0300

    Arrumando bugs de usuario e agendamento de adm

--- functions/validaAgendamento.php
+++ functions/validaAgendamento.php
@@ -7,18 +7,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'] ?? null;
 
     if ($acao && $id) {
-        $status = ($acao === 'finalizado' || $acao === 'cancelado') ? $acao : 'pendente';
+        if($acao === 'finalizado' || $acao === 'cancelado'){
+            $status = ($acao === 'finalizado' || $acao === 'cancelado') ? $acao : 'pendente';
 
-        $sql = "UPDATE agenda SET status_agenda = :status WHERE id_agenda = :id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':status', $status);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
+            $sql = "UPDATE agenda SET status_agenda = :status WHERE id_agenda = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':status', $status);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
 
-        header("Location: ../public/adm/horarios.php");
-        exit();
-    } else {
-        // redireciona ou mostra erro
+        }elseif($acao === 'cancelado_cliente'){
+            $sql = "DELETE FROM AGENDA where id_agenda = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+        }
     }
 }
+
+if($_SESSION['tipo_usuario'] == 'cliente'){
+    header("location: ../public/user/perfil.php");
+    exit();
+}elseif($_SESSION['tipo_usuario'] == 'administrador');
+    header("location: ../public/adm/horarios.php");
+    exit();
 ?>
